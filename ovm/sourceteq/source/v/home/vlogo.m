7c81420ae1f8375c1dd25cbbcda5ddd6c096f2c4
vauxhall <zero.griffin@gmail.com>
2016-01-13 17:42:54 -0600

    collection

--- ovm/sourceteq/source/v/home/vlogo.m
+++ ovm/sourceteq/source/v/home/vlogo.m
@@ -2,4 +2,29 @@
 
 @implementation vlogo
 
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setUserInteractionEnabled:NO];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setTranslatesAutoresizingMaskIntoConstraints:YES];
+    
+    UIImageView *icon = [[UIImageView alloc] init];
+    [icon setContentMode:UIViewContentModeCenter];
+    [icon setClipsToBounds:YES];
+    [icon setImage:[UIImage imageNamed:@"logo"]];
+    [icon setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:icon];
+    
+    NSDictionary *views = @{@"icon":icon};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[icon]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[icon]-0-|" options:0 metrics:metrics views:views]];
+    
+    return self;
+}
+
 @end
