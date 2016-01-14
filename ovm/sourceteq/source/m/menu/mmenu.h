1d691211a420f506a65dfcc0c56c2941f5577ab2
vauxhall <zero.griffin@gmail.com>
2016-01-14 13:47:03 -0600

    menu protocol

--- ovm/sourceteq/source/m/menu/mmenu.h
+++ ovm/sourceteq/source/m/menu/mmenu.h
@@ -1,13 +1,7 @@
-//
-//  mmenu.h
-//  ovm
-//
-//  Created by zero on 1/14/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import "appdel.h"
 
-#import <Foundation/Foundation.h>
+@interface mmenu:NSObject
 
-@interface mmenu : NSObject
+-(NSInteger)count;
 
 @end
--- ovm/sourceteq/source/m/menu/mmenu.m
+++ ovm/sourceteq/source/m/menu/mmenu.m
@@ -1,13 +1,26 @@
-//
-//  mmenu.m
-//  ovm
-//
-//  Created by zero on 1/14/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mmenu.h"
 
 @implementation mmenu
+{
+    NSArray *array;
+}
+
+-(instancetype)init
+{
+    self = [super init];
+    
+    array = [NSArray arrayWithObjects:nil];
+    
+    return self;
+}
+
+#pragma mark public
+
+-(NSInteger)count
+{
+    return array.count;
+}
+
+
 
 @end
