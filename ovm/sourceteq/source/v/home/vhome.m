8ded5d615939736636f5602dd79a38148744b521
vauxhall <zero.griffin@gmail.com>
2016-01-13 18:10:10 -0600

    col

--- ovm/sourceteq/source/v/home/vhome.m
+++ ovm/sourceteq/source/v/home/vhome.m
@@ -51,6 +51,21 @@
 #pragma mark -
 #pragma mark col del
 
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout referenceSizeForFooterInSection:(NSInteger)section
+{
+    return CGSizeMake(self.bounds.size.width, 50);
+}
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout referenceSizeForHeaderInSection:(NSInteger)section
+{
+    return CGSizeMake(self.bounds.size.width, 40);
+}
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    return CGSizeMake(self.bounds.size.width, 60);
+}
+
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
     return 1;
@@ -59,6 +74,22 @@
 -(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
 {
     return 0;
+}
+
+-(UICollectionReusableView*)collectionView:(UICollectionView*)col viewForSupplementaryElementOfKind:(NSString*)kind atIndexPath:(NSIndexPath*)index
+{
+    UICollectionReusableView *reusable;
+    
+    if(kind == UICollectionElementKindSectionHeader)
+    {
+        reusable = [col dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:headerid forIndexPath:index];
+    }
+    else
+    {
+        reusable = [col dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:footerid forIndexPath:index];
+    }
+    
+    return reusable;
 }
 
 -(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
