e9d2b9e56f19ab36fbccbbe512ce79f09cf0d59d
vauxhall <zero.griffin@gmail.com>
2016-01-14 14:17:08 -0600

    cel

--- ovm/sourceteq/source/v/home/vhomecelmenucel.m
+++ ovm/sourceteq/source/v/home/vhomecelmenucel.m
@@ -6,18 +6,21 @@
 {
     self = [super initWithFrame:frame];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor clearColor]];
     
     UILabel *lbl = [[UILabel alloc] init];
     [lbl setBackgroundColor:[UIColor clearColor]];
     [lbl setTextAlignment:NSTextAlignmentCenter];
     [lbl setFont:[UIFont fontWithName:fontname size:12]];
+    [lbl setUserInteractionEnabled:NO];
+    [lbl setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.lbl = lbl;
     
     UIImageView *icon = [[UIImageView alloc] init];
     [icon setClipsToBounds:YES];
     [icon setContentMode:UIViewContentModeScaleAspectFit];
     [icon setUserInteractionEnabled:NO];
     [icon setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.icon = icon;
     
     [self addSubview:icon];
     [self addSubview:lbl];
@@ -32,5 +35,37 @@
     
     return self;
 }
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self hover];
+}
+
+-(void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    [self hover];
+}
+
+#pragma mark fucntionality
+
+-(void)hover
+{
+    if(self.isSelected || self.isHighlighted)
+    {
+        [self.icon setTintColor:[UIColor whiteColor]];
+        [self.lbl setTextColor:[UIColor whiteColor]];
+        [self setBackgroundColor:colormain];
+    }
+    else
+    {
+        [self.icon setTintColor:colormain];
+        [self.lbl setTextColor:colormain];
+        [self setBackgroundColor:[UIColor clearColor]];
+    }
+}
+
+#pragma mark public
 
 @end
